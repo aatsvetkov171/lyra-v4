fc8d0340304316f1f342175151105a68c25b2027
tsvetkov <alextsvetkov1709@gmail.com>
2025-11-03 01:22:45 +0300

    router 4..

--- pkg/lyra/http1/views.go
+++ pkg/lyra/http1/views.go
@@ -1,11 +1,19 @@
 package http1
 
-func NotFound(request *Request) *Response
+func NotFound(request *Request) *Response {
+	response := NewResponse()
+	response.AddBody([]byte("<p>Lyyyyraaa</p>"))
+	return response //потом исправлю на правильное поведение
+}
 
-func MethodNotAllowed(request *Request) *Response
+func MethodNotAllowed(request *Request) *Response {
+	response := NewResponse()
+	response.AddBody([]byte("<p>Lyyyyraaa</p>"))
+	return response //потом исправлю на правильное поведение
+}
 
 func Hello(request *Request) *Response {
 	response := NewResponse()
-	response.AddBody([]byte("<p>Lyyyyraaa</p>"))
+	response.AddBody([]byte("<p>Lyyyyraaa Helloooo</p>"))
 	return response
 }
